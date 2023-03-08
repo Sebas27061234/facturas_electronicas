9096abef824869537217e8cef60efe6b61b533b2
sebas2706 <95267127+Sebas27061234@users.noreply.github.com>
2023-03-07 19:58:38 -0500

    DFGDFSU

--- factura.php
+++ factura.php
@@ -1,1 +1,18 @@
+<?php
+    $doc = new DOMDocument();
+    $doc->formatOutput = FALSE;
+    $doc->preserveWhiteSpace = TRUE;
+    $doc->encoding = 'utf-8';
 
+    $xml = '<?xml version="1.0" encoding="UTF-8"?>
+            <factura>
+                <fecha>2021-07-20</fecha>
+                <serie>F001</serie>
+                <correlativo>455</correlativo>
+            </factura>';
+
+    $doc->loadXML($xml);
+    $doc->save('FACTURA.XML');
+
+    echo "XML CREADO",
+?>
